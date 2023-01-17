72f1a67c4a85f7ed1b1ae797a6d9f38dbc1ae8d8
강진우 <wlsdn1252@naver.com>
2023-01-17 15:20:59 +0900

    실제 탭레이아웃, 뷰페이저 연결

--- app/src/main/java/com/example/selffragmentprectice/MainActivity.kt
+++ app/src/main/java/com/example/selffragmentprectice/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.selffragmentprectice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.selffragmentprectice.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mViewPagerAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 최초로 실행되면 MainViewPagerAdapter클래스를 담아주는데 supportFragmentManager로 담을거야
+        mViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+
+        // 메인 xml의 id가 mainViewPagerd인 프래그먼트의 어댑터는 mViewpagerAdapter가 될거다
+        mainViewPager.adapter = mViewPagerAdapter
+
+        // 메인 xml에서 id가 tabLayout인 것이랑 mainViewPager인 것을 연결
+        // 실제 화면에 나타님
+        tabLayout.setupWithViewPager(mainViewPager)
+
     }
 }
