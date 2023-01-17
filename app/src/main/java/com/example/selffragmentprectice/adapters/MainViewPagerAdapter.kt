e2ec0e6658a1b5d8e8a44c29aa923bff447ec0b8
강진우 <wlsdn1252@naver.com>
2023-01-17 15:22:24 +0900

    탭레이아웃 제목 설정

--- app/src/main/java/com/example/selffragmentprectice/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/selffragmentprectice/adapters/MainViewPagerAdapter.kt
@@ -8,6 +8,14 @@ import com.example.selffragmentprectice.fragments.SecondFragment
 
 class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
+    // 탭 레이아웃 제목 설정
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 -> "첫번쨰"
+            else -> "두번째"
+        }
+    }
+
     override fun getItem(position: Int): Fragment {
         return when(position){
             0 -> FirstFragment()
